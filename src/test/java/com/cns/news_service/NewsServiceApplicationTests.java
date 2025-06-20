da902c53c1c6652499f837c255e1d9d4cf60048f
Jiyoung Park <pjy2163@sookmyung.ac.kr>
2025-06-20 10:37:33 +0900

    Initial commit

---
+++ src/test/java/com/cns/news_service/NewsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cns.news_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
