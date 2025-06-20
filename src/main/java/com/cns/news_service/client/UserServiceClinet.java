147c9ba9a6e4e01fec247d6e34ca4d7ac8afae93
Jiyoung Park <pjy2163@sookmyung.ac.kr>
2025-06-20 17:09:00 +0900

    feat: user-service open feign 관련 dto, 클래스 구현

---
+++ src/main/java/com/cns/news_service/client/UserServiceClinet.java
@@ -0,0 +1,19 @@
+package com.cns.news_service.client;
+
+import com.cns.news_service.dto.response.FavoriteResponseDto;
+import com.cns.news_service.dto.response.InterestResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient("User-Service")
+public interface UserServiceClinet {
+    @GetMapping("/favorites/{userId}")
+    List<FavoriteResponseDto> getFavorites(@PathVariable("userId") Long userId);
+
+    @GetMapping("/interest/{userId}")
+    List<InterestResponseDto> getInterests(@PathVariable("userId") Long userId);
+
+}
