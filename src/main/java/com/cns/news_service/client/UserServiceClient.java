6e75712eb2a9aa0450fb685bedf151db0dcaff9d
Jiyoung Park <pjy2163@sookmyung.ac.kr>
2025-06-24 23:20:11 +0900

    feat: open feign config 구현 (0.0.4)

--- src/main/java/com/cns/news_service/client/UserServiceClient.java
+++ src/main/java/com/cns/news_service/client/UserServiceClient.java
@@ -1,5 +1,6 @@
 package com.cns.news_service.client;
 
+import com.cns.news_service.client.config.FeignConfig;
 import com.cns.news_service.dto.response.FavoriteResponseDto;
 import com.cns.news_service.dto.response.InterestResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "backend-user-service", url = "http://backend-user-service:8080")
+@FeignClient(name = "backend-user-service", url = "http://backend-user-service:8080", configuration = FeignConfig.class)
 public interface UserServiceClient {
     @GetMapping("/api/user/v1/favorites/{userId}")
     List<FavoriteResponseDto> getFavorites(@PathVariable("userId") Long userId);
---
+++ src/main/java/com/cns/news_service/client/config/FeignConfig.java
@@ -0,0 +1,28 @@
+package com.cns.news_service.client.config;
+
+import com.cns.news_service.common.util.GatewayRequestHeaderUtils;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Configuration
+public class FeignConfig implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+        if (attributes != null) {
+            String userId = GatewayRequestHeaderUtils.getRequestHeaderParamAsString("X-Auth-UserId");
+            String clientAddr = GatewayRequestHeaderUtils.getRequestHeaderParamAsString ("X-Client-Address");
+            String clientDevice = GatewayRequestHeaderUtils.getRequestHeaderParamAsString("X-Client-Device");
+
+            if (userId != null) requestTemplate.header("X-Auth-UserId", userId);
+            if (clientAddr != null) requestTemplate.header("X-Client-Address", clientAddr);
+            if (clientDevice != null) requestTemplate.header("X-Client-Device", clientDevice);
+        }
+
+    }
+}
