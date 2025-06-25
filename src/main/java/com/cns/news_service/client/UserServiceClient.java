2b1efa04e5d4d6985477cba5577f6aa101967c0f
Jiyoung Park <pjy2163@sookmyung.ac.kr>
2025-06-25 09:12:19 +0900

    refactor: 로드밸런서 추가, config 주석 처리

--- src/main/java/com/cns/news_service/client/UserServiceClient.java
+++ src/main/java/com/cns/news_service/client/UserServiceClient.java
@@ -1,6 +1,5 @@
 package com.cns.news_service.client;
 
-import com.cns.news_service.client.config.FeignConfig;
 import com.cns.news_service.dto.response.FavoriteResponseDto;
 import com.cns.news_service.dto.response.InterestResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -9,12 +8,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "backend-user-service", url = "http://backend-user-service:8080", configuration = FeignConfig.class)
+@FeignClient(name = "backend-user-service", url = "http://backend-user-service:8080" , path = "/api/user/v1")
 public interface UserServiceClient {
-    @GetMapping("/api/user/v1/favorites/{userId}")
+    @GetMapping("/favorites/{userId}")
     List<FavoriteResponseDto> getFavorites(@PathVariable("userId") Long userId);
 
-    @GetMapping("/api/user/v1/interest/{userId}")
+    @GetMapping("/interest/{userId}")
     List<InterestResponseDto> getInterests(@PathVariable("userId") Long userId);
 
 }
